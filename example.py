13f2fcb47f80ff1ee76df40316786129dc514591
Selva Kumar N Nambi N <snambi@AD.MSYSTECHNOLOGIES.COM>
2020-11-22 07:43:15 +0530

    Run example.py to start

---
+++ example.py
@@ -0,0 +1,16 @@
+from messageBus import busClient
+from messageBus.busClient import Producer, Consumer
+
+
+def main():
+    busStart = busClient('kafka')
+    prod_obj = Producer(busStart)
+    print(prod_obj.send('sampl', b"test message"))
+    cons_obj = Consumer(busStart)
+    print(cons_obj.receive(['sampl']))
+
+
+if __name__ == "__main__":
+    main()
+
+
